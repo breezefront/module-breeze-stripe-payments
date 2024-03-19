6d9e17fd977fced7fe8372128a9fa6f1e2223434
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2024-03-19 17:07:01 +0200

    Remove unused code

--- view/frontend/web/js/stripe_payments_express.js
+++ view/frontend/web/js/stripe_payments_express.js
@@ -2,6 +2,4 @@ define(['jquery'], ($) => {
     'use strict';
 
     $.breezemap.__register('StripeIntegration_Payments/js/stripe_payments_express');
-
-    $(document).trigger('breeze:disable-turbo');
 });
