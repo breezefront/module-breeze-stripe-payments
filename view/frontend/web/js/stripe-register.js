8dd4d3c65f2a290194e32ca5aa13f94d02c393cc
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2024-03-19 16:00:41 +0200

    Update to the latest breeze

--- view/frontend/web/js/stripe-register.js
+++ view/frontend/web/js/stripe-register.js
@@ -1,6 +1,5 @@
 define([], function () {
     'use strict';
 
-    $.breezemap['stripejs'] = require('https://js.stripe.com/v3/');
     $.breezemap['StripeIntegration_Payments/js/stripe'] = window.stripe;
 });
