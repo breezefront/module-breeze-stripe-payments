3ed36fb7bd7049c870592a831c205e7ca7697502
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2024-03-20 15:00:37 +0200

    Payment methods page support added.

---
+++ view/frontend/web/js/actions-register.js
@@ -0,0 +1,9 @@
+define([
+    'jquery'
+], function ($) {
+    'use strict';
+
+    $.breezemap.__register('StripeIntegration_Payments/js/action/list-payment-methods', 0);
+    $.breezemap.__register('StripeIntegration_Payments/js/action/delete-payment-method', 1);
+    $.breezemap.__register('StripeIntegration_Payments/js/action/add-payment-method', 2);
+});
---
+++ view/frontend/web/js/ui_setup_element.js
@@ -0,0 +1,5 @@
+define([], function () {
+    'use strict';
+
+    $.breezemap.__register('StripeIntegration_Payments/js/view/ui_components/setup_element');
+});
