f2273046ad0d454c2266515814481660d71f574f
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2024-04-30 16:22:46 +0300

    Lazy initialization of stripe buttons

--- view/frontend/web/js/stripe_payments_express.js
+++ view/frontend/web/js/stripe_payments_express.js
@@ -2,4 +2,10 @@ define(['jquery'], ($) => {
     'use strict';
 
     $.breezemap.__register('StripeIntegration_Payments/js/stripe_payments_express');
+
+    $.mixin('StripeIntegration_Payments/js/stripe_payments_express', {
+        initStripeExpress: function (parent) {
+            $.lazy(() => parent.apply(this, Array.prototype.slice.call(arguments, 1)));
+        }
+    });
 });
